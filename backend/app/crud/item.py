c5a5c74c43738a290be3f4cbc5008e835100bc80
Idah Anyango Ologi <ologiidah@gmail.com>
2026-07-09 16:18:09 +0300

    Add get_item and delete_item functions
    Added functions to retrieve and delete items from the database.

--- backend/app/crud/item.py
+++ backend/app/crud/item.py
@@ -6,13 +6,25 @@ from app.schemas.item import ItemCreate
 
 def create_item(db: Session, item: ItemCreate):
     db_item = Item(**item.model_dump())
-
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
-
     return db_item
 
 
 def get_items(db: Session):
     return db.query(Item).all()
+
+
+def get_item(db: Session, item_id):
+    return db.query(Item).filter(Item.id == item_id).first()
+
+
+def delete_item(db: Session, item_id):
+    item = get_item(db, item_id)
+
+    if item:
+        db.delete(item)
+        db.commit()
+
+    return item
